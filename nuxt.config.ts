0ef60445ccf64a51171ff679e8d91b742df930eb
Victor Zellweger <vazellweg@gmail.com>
2025-09-24 16:38:50 -0700

    add supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/supabase'
+  ]
 })
