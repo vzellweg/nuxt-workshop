8685eb4071dc3cb0aa91457a94c6e92cd2917aac
Victor Zellweger <vazellweg@gmail.com>
2025-09-25 14:35:57 -0700

    app + landing page basic layout complete

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,31 @@
   <UApp>
     <!-- Will tell screen readers what route is currently active and when it has changed-->
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+
+    <!-- Navbar -->
+    <UHeader>
+      <template #left>
+        <h1 class="text-xl font-semibold">Workshopping Platform</h1>
+      </template>
+
+      <template #right>
+        <USkeleton class="h-8 w-8 rounded-full" />
+      </template>
+    </UHeader>
+
+    <UMain class="flex-1 flex flex-col">
+      <div class="flex-1 flex flex-col md:px-24 md:py-16 sm:py-8">
+        <NuxtPage />
+      </div>
+
+      <!-- Show footer at the bottom of the page when no overflowing content is present -->
+      <UFooter>
+        <template #left>
+          <p class="text-muted text-sm">
+            Victor Zellweger © {{ new Date().getFullYear() }}
+          </p>
+        </template>
+      </UFooter>
+    </UMain>
   </UApp>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <!-- List of available document workshops, message saying no workshops available -->
-  <UContainer class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold">Document Workshops</h1>
-    <p>No workshops available</p>
-    <UButton class="mt-4">Create Workshop</UButton>
+  <UContainer>
+    <h1 class="text-4xl text-center mb-12">Default Workshopping Section</h1>
+    <div class="flex flex-row align-center">
+      <div class="w-3/4">
+        <h2 class="text-2xl font-bold">Document Workshops</h2>
+        <p>No workshops available</p>
+        <UButton class="mt-4">Create Workshop</UButton>
+      </div>
+      <div class="w-1/4">
+        <h2 class="text-2xl font-bold">Recent Activity</h2>
+        <p>No activity available</p>
+      </div>
+    </div>
   </UContainer>
 </template>
 
