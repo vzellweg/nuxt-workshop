d7c4e98015c980956131be42b65f43a6d41b04ee
Victor Zellweger <vazellweg@gmail.com>
2025-10-01 12:20:56 -0700

    finish CRUD operations of useWorkshops composable

--- app/composables/useWorkshop.ts
+++ app/composables/useWorkshop.ts
@@ -1,11 +1,71 @@
-import type { Workshop } from "~/types";
+import type { Workshop, WorkshopKind } from "~/types";
 import { MOCK_WORKSHOP } from "./mockData";
 
 const useWorkshops = () => {
-  const workshop = ref<Workshop>(MOCK_WORKSHOP);
+  const workshops = ref<Workshop[]>([MOCK_WORKSHOP]);
+
+  const createWorkshop = (
+    title: string,
+    description: string,
+    content: object,
+    // begin optional / default params
+    workshopKind: WorkshopKind = "document",
+    published?: boolean
+  ) => {
+    const id = workshops.value.length + 1;
+    const draftJson = JSON.stringify(content);
+    const publishedJson = published ? draftJson : null;
+    const workshop: Workshop = {
+      id: id.toString(),
+      title,
+      description,
+      kind: workshopKind,
+      slug: null,
+      ownerId: "johndoe",
+      draftJson,
+      publishedJson,
+      isPublished: published ?? false,
+      publishedAt: published ? new Date().toISOString() : null,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      isDeleted: false,
+      deletedAt: null,
+    };
+    workshops.value.push(workshop);
+    // Should I return the workshop referenced from inside the workshops array so it is a reactive Proxy Object?
+    return workshops.value[workshops.value.length - 1];
+  };
+
+  const getWorkshop = (id: string) => {
+    return workshops.value.find((workshop) => workshop.id === id);
+  };
+
+  const updateWorkshop = (workshop: Workshop) => {
+    const index = workshops.value.findIndex((w) => w.id === workshop.id);
+    if (index !== -1) {
+      workshops.value[index] = workshop;
+    } else {
+      throw new Error("Workshop not found");
+    }
+    return workshops.value[index];
+  };
+
+  const deleteWorkshop = (id: string) => {
+    const index = workshops.value.findIndex((w) => w.id === id);
+    if (index !== -1) {
+      workshops.value[index]!.isDeleted = true;
+      workshops.value[index]!.deletedAt = new Date().toISOString();
+    } else {
+      throw new Error("Workshop not found");
+    }
+  };
 
   return {
-    workshop,
+    workshops,
+    createWorkshop,
+    getWorkshop,
+    updateWorkshop,
+    deleteWorkshop,
   };
 };
 
